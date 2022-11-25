e1e2d5ec6a06439fcdebd0be9476e0f60fd466f1
AlexandrKruts <kruts704@gmail.com>
2022-11-25 18:31:55 +0200

    Is there something wrong

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Users from "./routes/Users";
+import User from "./routes/Hotels";
+import Hotels from "./routes/Hotels";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    children: [
+        {
+            path: "",
+            element: <Home />,
+          },
+        {
+            path: "users",
+            element: <Users />,
+          },
+          {
+            path: "users/:userId",
+            element: <User />
+          },
+          {
+            path: "hotels",
+            element: <Hotels />,
+          },
+      ]
+  },
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
+
 reportWebVitals();
